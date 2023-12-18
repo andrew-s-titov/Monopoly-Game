cab17ea2bca60d67602aad2ea4098cf7b42b052c
andrew.s.titov <andrew.s.titov@gmail.com>
2023-12-18 23:32:53 +0100

    feat: new year edition snowfall

--- frontend/src/components/GameContent.tsx
+++ frontend/src/components/GameContent.tsx
@@ -1,10 +1,14 @@
 import GameMap from "./GameMap";
 import PlayerContainer from "./player/PlayerContainer";
+import Snowfall from "react-snowfall";
 
 const GameContent = () => {
 
   return (
     <div className='game-content-container'>
+      <Snowfall
+        snowflakeCount={250}
+      />
       <PlayerContainer/>
       <GameMap/>
     </div>
