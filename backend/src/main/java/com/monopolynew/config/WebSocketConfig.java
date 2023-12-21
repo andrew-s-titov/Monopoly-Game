b3ad95eb00ef7082cdcc0d0b9512067a51795414
andrew.s.titov <andrew.s.titov@gmail.com>
2023-12-21 19:03:55 +0100

    refactor: remove java server endpoint explorer

--- backend/src/main/java/com/monopolynew/config/WebSocketConfig.java
+++ backend/src/main/java/com/monopolynew/config/WebSocketConfig.java
@@ -3,12 +3,10 @@ package com.monopolynew.config;
 import com.monopolynew.websocket.GameWebSocketHandler;
 import com.monopolynew.websocket.GameWsInterceptor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 @EnableWebSocket
@@ -17,11 +15,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     private final GameWebSocketHandler gameWebsocketHandler;
     private final GameWsInterceptor handshakeInterceptor;
-
-    @Bean
-    public ServerEndpointExporter endpointExporter() {
-        return new ServerEndpointExporter();
-    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
