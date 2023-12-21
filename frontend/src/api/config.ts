49d5829500653587175674b598020a22d1883c61
andrew.s.titov <andrew.s.titov@gmail.com>
2023-12-21 18:48:12 +0100

    refactor: use Spring websockets instead of Java native

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -6,10 +6,10 @@ export const BE_ENDPOINT = document.location.origin;
 
 export const getWebsocketUrl = () => {
   const wsUri = getWebsocketEndpoint();
-  return `${wsUri}/${getLoggedInUserId()}`;
+  return `${wsUri}?user_id=${getLoggedInUserId()}`;
 }
 
 const getWebsocketEndpoint = () => {
   const host = BE_ENDPOINT.split('://')[1];
-  return `${BE_ENDPOINT.startsWith('https') ? 'wss' : 'ws'}://${host}`;
+  return `${BE_ENDPOINT.startsWith('https') ? 'wss' : 'ws'}://${host}/ws`;
 }
